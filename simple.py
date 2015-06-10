aadc7b8cdec68e2c690d1da66b69d41bcc20d27e
ryaninhust <yuanbwhust@gmail.com>
2015-06-10 18:13:25 +0800

    Add more complex simple

--- simple.py
+++ simple.py
@@ -1,15 +1,18 @@
 from ada_dnn import DNN
 import numpy
+from sklearn.externals import joblib
 
 #you need numpy data file
 train_x = numpy.load('X.npy')
 train_y = numpy.load('Y.npy')
 
 
-dnn = DNN([69,512, 39],
-          learning_rate_decays=0.98, learning_rate=0.02,
-          batch_size=256, epochs=10)
+dnn = DNN([69,2048, 2048, 2048, 2048, 48],
+          learning_rate=0.02,
+          dropouts=0.25,
+          batch_size=256, epochs=30)
 
 dnn.fit(train_x, train_y)
+joblib.dump(dnn, 'model')
 dnn.predict_proba(train_x)
 
